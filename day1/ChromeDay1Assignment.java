44d09062ec70d61fb0fb01cdec54a63c76db3eab
Monapriya386 <34203455+Monapriya386@users.noreply.github.com>
2024-06-24 00:06:42 +0530

    Add files via upload
    Day1 - Mona Assignment

---
+++ day1/ChromeDay1Assignment.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class ChromeDay1Assignment {
+
+	public static void main(String[] args) {
+	
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int release = 2008;
+		char shortcutKey = 'C';
+		
+	System.out.println(version);
+	System.out.println(developer);
+	System.out.println(isBeta);
+	System.out.println(release);
+	System.out.println(shortcutKey);
+	
+	}
+	
+}
